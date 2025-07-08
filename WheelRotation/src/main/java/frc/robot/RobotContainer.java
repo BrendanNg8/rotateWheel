11bae08626ba6e7eaad31ff127e9547522539ae8
Kanna <kortizcheng@stu.socsd.org>
2025-07-08 15:21:25 -0400

    Finished Swerve

---
+++ WheelRotation/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.rotationCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.rotationSub;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  rotationCommand m_rotationCommand;
+  rotationSub m_rotationSub;
+  Joystick joystick;
+  CANcoder cancode;
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    joystick = new Joystick(0); // Assuming joystick is on port 0
+    cancode = new CANcoder(9, "Drivetrain"); // Assuming CANcoder is on ID 1 in the "Drivetrain" CAN bus
+    m_rotationSub = new rotationSub(joystick,cancode);
+    m_rotationCommand = new rotationCommand(m_rotationSub, joystick,cancode);
+    
+    configureBindings();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+}
---
+++ WheelRotation/src/main/java/frc/robot/subsystems/rotationSub.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class rotationSub extends SubsystemBase {
+  /** Creates a new rotationSub. */
+  TalonFX motor;
+  double kp = 0.005;
+  double ki = 0.0;
+  double kd = 0.00000000;
+  Joystick joy;
+  // PID controller for the motor
+  // Adjust these values based on your system's requirements
+  PIDController pid; 
+  
+
+  CANcoder encoder;
+  public rotationSub(Joystick j, CANcoder c) {
+    motor = new TalonFX(1, "Drivetrain");
+    pid = new PIDController(kp, ki, kd);
+    pid.enableContinuousInput(0, 360);
+    joy = j;
+    encoder = c;
+  }
+  public void setPID(double p, double i, double d) {
+    // Set the PID coefficients
+    kp = p;
+    ki = i;
+    kd = d;
+    pid.setPID(kp, ki, kd);
+  }
+  public void setMotorSpeed(double speed) {
+    // Set the motor speed
+    motor.set(speed);
+  }
+  public double getEncoderPosition() {
+    // Get the encoder position
+    return ((((encoder.getPosition().getValueAsDouble()%1)+1)%1)*360);
+  }
+  /* rotateMotor(Joystick j, CANcoder c,) {
+    // This method will rotate the motor based on joystick input
+    double speed = j.getY(); // Assuming Y-axis controls the speed
+    motor.set(speed);
+    
+    // Optionally, you can read the encoder value
+    double position = c.getPosition();
+    System.out.println("Encoder Position: " + position);
+  } */
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    double currentAngle = getEncoderPosition();
+    double targetAngle = -joy.getDirectionDegrees()+180; // Example target angle
+    double output = pid.calculate(currentAngle, targetAngle);
+    setMotorSpeed(output);
+  }
+}
